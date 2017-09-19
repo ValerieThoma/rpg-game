cca1a0578dcb6387f90fd650339377c7837c1791
Valerie Jane <31670582+ValerieThoma@users.noreply.github.com>
2017-09-19 08:30:31 -0400

    Delete madlib.py

--- python_prt1/madlib.py
+++
@@ -1,8 +0,0 @@
- #3. Madlib
- #The quick brown fox jumped over the lazy dog.
-
-adverb = raw_input("May I have an adverb?\n >")
-color = raw_input("May I have a color?\n >")
-adjective = raw_input("May I have an adjective?\n>")
-animal = raw_input("Will you tell me your favorite animal?\n")
-print "The %r %r fox jumped over the %r %r." % (adverb, color, adjective, animal)
