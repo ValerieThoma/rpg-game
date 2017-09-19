7dc293177bd8a8ab53ea24178cfb7123b1ced88e
Valerie Jane <31670582+ValerieThoma@users.noreply.github.com>
2017-09-19 08:30:12 -0400

    Delete hello.py

--- python_prt1/hello.py
+++
@@ -1,9 +0,0 @@
- # 1. Hello, you!
-name = raw_input("What is your name? ")
-print "Hello, " + name 
-
-#  # 2. Hello, you! 
-name = raw_input("What is your name? ")
-nameLength = str(len(name))
-print "HELLO " + name.upper() + "!" + " YOUR NAME HAS " + nameLength.upper() + " LETTERS IN IT. AWESOME!" 
-
