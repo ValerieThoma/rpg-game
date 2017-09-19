a8494a54749fccbb7335414999872d150f71cfbc
Valerie Jane <31670582+ValerieThoma@users.noreply.github.com>
2017-09-19 08:29:36 -0400

    Delete day_of_the_week.py

--- python_prt1/day_of_the_week.py
+++
@@ -1,6 +0,0 @@
-#4. Day of the Week
-#if Sun - Sat is set equal to 0 - 6
-userInput = int(raw_input("Pick a number between 0 and 6: "))
-todayIs = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-print "Today is " + todayIs[userInput]
