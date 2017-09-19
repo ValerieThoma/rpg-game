bd90c0ac2ac1a87e1a8b44e1682cceda4daca0eb
Valerie Jane <31670582+ValerieThoma@users.noreply.github.com>
2017-09-19 08:30:57 -0400

    Delete work_or_sleep_in.py

--- python_prt1/work_or_sleep_in.py
+++
@@ -1,8 +0,0 @@
-# 5. Work or Sleep In? 
-#if Sun-Sat is set equal to 0 - 6
-today = int(raw_input("Pick a number between 0 and 6: "))
-
-if (today == 0 or today == 6):
-	print "Sleep, sweet child!"
-else:
-	print "You better go to work!"
